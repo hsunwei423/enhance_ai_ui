4cf4bf09945f395ad7ee7073b1dc56522cc9541c
Johnson <weihsun.chen@nogle.com>
2021-12-02 18:30:17 +0800

    adjust header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,44 @@
+import { Link, useLocation } from "react-router-dom";
+
 import Logo from "assets/imgs/logo.svg";
+import SignupImg from "assets/imgs/signup.png";
+
+import { ROUTE_LIST } from "consts";
 
 export default function Header() {
+  const location = useLocation();
+
+  const renderLinkList = () => {
+    return (
+      <ul className="flex gap-x-1">
+        {ROUTE_LIST.map((d) => (
+          <li
+            className={`px-4 flex items-center justify-center rounded-lg ${
+              d.path === location.pathname &&
+              "bg-gradient-to-r from-green-400 to-green-500 h-12 text-white"
+            }`}
+            key={d.name}
+          >
+            <Link to={d.path}>{d.name}</Link>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
-    <div className="container mx-auto">
-      <img className="object-contain" src={Logo} alt="logo" />
+    <div className="border-b">
+      <header className="max-w-screen-lg mx-auto flex items-center justify-between h-20">
+        <img className="object-contain" src={Logo} alt="logo" />
+
+        <div className="flex items-center">
+          <nav>{renderLinkList()}</nav>
+          <div className="bg-gradient-to-r from-purple-600 to-purple-800 h-12 w-40 rounded-2xl flex items-center justify-center gap-2 ml-16">
+            <img src={SignupImg} alt="signup" />
+            <span className="font-bold text-white">Free Signup</span>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
---
+++ src/consts/index.ts
@@ -0,0 +1,22 @@
+export const ROUTE_LIST = [
+  {
+    path: "/",
+    name: "Home",
+  },
+  {
+    path: "/features",
+    name: "Features",
+  },
+  {
+    path: "/pricing",
+    name: "Pricing",
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+  },
+  {
+    path: "/login",
+    name: "Login",
+  },
+];
