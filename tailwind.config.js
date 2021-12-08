e8471ba6e609e81a9a051ee942a330507f1518e7
Johnson <weihsun.chen@nogle.com>
2021-12-08 11:47:28 +0800

    remove cra normalize css & fix tailwind purge settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   // purge: ["/src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  purge: ["./public/index.html"],
+  purge: {
+    enabled: true,
+    layers: ["components", "pages"],
+    content: ["/src/**/*.{js,jsx,ts,tsx}"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
